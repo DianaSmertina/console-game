44dd54686c39d393305f18cf44071bd72f083ac5
DianaSmertina <am.dianasmertina@gmail.com>
2023-08-05 12:49:58 +0400

    feat: add args checks

--- application/app.js
+++ application/app.js
@@ -7,12 +7,25 @@ var rules_1 = require("./components/rules/rules");
 var App = /** @class */ (function () {
     function App() {
     }
-    App.prototype.run = function () {
-        App.gameMoves = process.argv.slice(2);
+    App.prototype.checkGameMoves = function () {
+        var args = new Set(process.argv.slice(2));
+        if (args.size >= 3 && args.size % 2 === 1) {
+            App.gameMoves = Array.from(args);
+            return true;
+        }
+        console.log('Please enter an odd number of arguments >= 3');
+        process.exit();
+    };
+    App.prototype.startGame = function () {
         new computer_1.default().chooseCompMove();
         new protection_1.default().generateHMAC();
         new rules_1.default().createWinData();
         new menu_1.default().showMenu();
+    };
+    App.prototype.run = function () {
+        if (this.checkGameMoves()) {
+            this.startGame();
+        }
     };
     return App;
 }());
--- application/app.ts
+++ application/app.ts
@@ -6,12 +6,27 @@ import Rules from "./components/rules/rules";
 class App {
     static gameMoves: Array<string>;
 
-    run() {
-        App.gameMoves = process.argv.slice(2);
+    checkGameMoves() {
+        const args = new Set(process.argv.slice(2));
+        if (args.size >= 3 && args.size % 2 === 1) {
+            App.gameMoves = Array.from(args);
+            return true;
+        }
+        console.log('Please enter an odd number of arguments >= 3');
+        process.exit();
+    }
+
+    startGame() {
         new Computer().chooseCompMove();
         new Protection().generateHMAC();
         new Rules().createWinData();
         new Menu().showMenu();
+    }
+
+    run() {
+        if (this.checkGameMoves()) {
+           this.startGame();
+        }
     }
 }
 
